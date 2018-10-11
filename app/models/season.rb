e7146ef82415413131262eb66b9a527ce7d00158
kelligivens <kelligivens@live.co.za>
2018-10-11 11:08:16 -0400

    Change database

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,10 +1,4 @@
 class Season < ApplicationRecord
   belongs_to :page
-  
-   SEASONS = {
-    'winter': 'winter',
-    'spring': 'spring',
-    'summer': 'summer',
-    'fall': 'fall',
-   }
+  has_many : seasons
 end
